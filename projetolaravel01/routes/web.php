5c29fc5c80cb9ce9d9df86dd430eb2ba1866cb7c
Maicon Lino <maicon@lifecti.com.br>
2023-02-22 09:15:19 -0300

    Blade avançado

--- projetolaravel01/routes/web.php
+++ projetolaravel01/routes/web.php
@@ -16,7 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $nome = "Maicon";
     $idade = 33;
-    return view('welcome', ["nome" => $nome, "idade" =>$idade, "profissao" => "Programador"]);
+
+    $arr = [10,20,30,40,50];
+
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view(
+        'welcome',
+        ["nome" => $nome,
+         "idade" =>$idade,
+         "profissao" => "Programador",
+         "arr" => $arr,
+         "nomes" => $nomes
+        ]);
 });
 
 Route::get('/contact', function () {
