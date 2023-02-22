ec50e7d908075d0249dd555e6bcc10ca6980e741
Maicon Lino <maicon@lifecti.com.br>
2023-02-22 09:50:13 -0300

    Arquivos estáticos

--- projetolaravel01/resources/views/products.blade.php
+++ projetolaravel01/resources/views/products.blade.php
@@ -1,2 +1,23 @@
-<h1>Tela de produtos</h1>
-<a href="/">Voltar para home</a>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="/css/styles.css" />
+    <script src="js/scripts.js"></script>
+</head>
+
+<body>
+    <h1>Tela de produtos</h1>
+    <a href="/">Voltar para home</a>
+</body>
+
+</html>
